aee34a0039229a3f40aef2af2d9219bda02351ba
Carlos Disher Luján Parra <clujan@bancodebogota.com.co>
2024-08-03 14:19:06 -0500

    add: util function planet namer

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
-from typing import Union
+from random import choice
 from datetime import datetime
+from typing import Union
 from .decorators import caster
 
 
@@ -15,3 +16,26 @@ def replace_to_decimal(obj: any) -> Union[Decimal, any]:
 
 
 cast_datetime_to_isoformat = caster(type=datetime)(datetime.isoformat)
+
+
+def planet_namer() -> str:
+    prefixes = [
+        "Zar", "Vor", "Xan", "Ald", "Bel", "Cor", "Dru", "El", "Fen", "Gar",
+        "Hor", "Ith", "Jar", "Kor", "Lir", "Mar", "Nor", "Or", "Per", "Qor",
+        "Ryn", "Syr", "Tor", "Uth", "Val", "Wor", "Xil", "Yar", "Zel", "New"
+    ]
+    suffixes = [
+        "donia", "thar", "phos", "lorn", "cara", "nix", "tia", "bex", "dil",
+        "mar", "nus", "ram", "ser", "tor", "vox", "xis", "yra", "zan", "cion",
+        "dros", "goth", "hoth", "ith", "kros", "lyn", "mir", "noth", "por",
+        "quor", "rith", " II", " III", " X", " Prime", " Major", " Minor"
+    ]
+    infixes = [
+        "ar", "or", "ur", "al", "il", "an", "on", "el", "un", "in", "en", "ol",
+        "ir", "ur", "ys", "us", "as", "os", "is", "es"
+    ]
+    prefix = choice(prefixes)
+    infix = choice(infixes)
+    suffix = choice(suffixes)
+    planet_name = prefix + infix + suffix
+    return planet_name
