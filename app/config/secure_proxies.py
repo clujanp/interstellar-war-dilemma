3e12a5da6707abdec5e689b94b7038c8032cad47
Carlos Disher Luján Parra <clujan@bancodebogota.com.co>
2024-08-29 20:30:00 -0500

    improve: add access for proxy in package and add proxies config

---
+++ app/config/secure_proxies.py
@@ -0,0 +1,32 @@
+from app.core.domain.models import Civilization, Planet
+from app.core.domain.services.memories import MemoriesServiceWrapper
+
+SECURITY_PROXY = {
+    Civilization: {
+        'readable_attrs': ['name'],
+        'writable_attrs': [],
+        'accessible_methods': []
+    },
+    Planet: {
+        'readable_attrs': ['name', 'cost'],
+        'writable_attrs': [],
+        'accessible_methods': []
+    },
+    MemoriesServiceWrapper: {
+        'readable_attrs': ['length'],
+        'writable_attrs': [],
+        'accessible_methods': [
+            'civilizations',
+            'opponents',
+            'skirmishes_count',
+            'last_position',
+            'last_score',
+            'cooperations',
+            'aggressions',
+            'conquests',
+            'hits',
+            'loss',
+            'mistakes',
+        ]
+    },
+}
