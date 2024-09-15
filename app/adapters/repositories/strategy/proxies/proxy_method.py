c2b15aaa33ed977eb90fea132ce83c22a0b40f4f
Carlos Disher Luján Parra <clujan@bancodebogota.com.co>
2024-09-15 00:31:19 -0500

    improve: caching in get method in proxy

--- app/adapters/repositories/strategy/proxies/proxy_method.py
+++ app/adapters/repositories/strategy/proxies/proxy_method.py
@@ -1,6 +1,7 @@
 from typing import List, Callable
 from app.config.messages import ERR_SECURE_PROXY as ERR_MSG
 from app.infraestructure.logging import logger
+from app.utils.decorators import cached
 from .exceptions import RestrictedAccessError
 
 
@@ -15,10 +16,13 @@ class MethodAccessibleProxy:
         self._accessible_methods = accessible_methods
         self._method_wrapper = method_wrapper
 
+    @cached
     def __getattr__(self, name: str) -> Callable:
         if name in self._accessible_methods:
             if self._method_wrapper is None:
                 return getattr(self._obj, name)
+            logger.debug(ERR_MSG['debug_metd_warps'].format(
+                name, self._obj.__repr__()))
             return self._method_wrapper(getattr(self._obj, name))
         else:
             raise RestrictedAccessError(ERR_MSG['acc_restric'].format(name))
