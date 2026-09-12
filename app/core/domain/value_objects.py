ed787ee76f904000d8e67c3c2e0479adb5bfa102
Carlos Disher Luján Parra <cdlujanp@gmail.com>
2026-09-12 16:49:48 -0500

    feat: improve desicion as numeric value operable and reversible

--- app/core/domain/value_objects.py
+++ app/core/domain/value_objects.py
@@ -1,11 +1,19 @@
-from enum import Enum
+from enum import IntEnum
 
 
-class Desicion(int):
-    ...
-
-
-class SkirmishDecision(Desicion, Enum):
+class Decision(IntEnum):
     COOPERATE = 1
     ATTACK = 2
     NONE = 0
+
+
+class SkirmishResult(IntEnum):
+    COOPERATION = 4
+    TREASON_A = 7
+    TREASON_B = 5
+    CONFLICT = 8
+    ASSIMILATION_A = 3
+    ASSIMILATION_B = 1
+    MASSACRE_A = 6
+    MASSACRE_B = 2
+    LOOSE = 0
--- tests/unit/test_skirmish_desicion_calculation.py
+++ tests/unit/test_skirmish_desicion_calculation.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from app.core.domain.value_objects import SkirmishDecision as SD
+from app.core.domain.value_objects import Decision as D, SkirmishResult as SR
 
 
 class TestSkirmishDesicionCalculation(TestCase):
@@ -8,28 +8,45 @@ class TestSkirmishDesicionCalculation(TestCase):
     def setUp(self):
         """Set up cases."""
 
+    def operation(self, first: D, second: D) -> SR:
+        """Placeholder for operation test."""
+        return SR(first * len(D) + second)
+
+    def reverse(self, combined: SR) -> tuple[D, D]:
+        """Reverse the operation to get the original decisions."""
+        first, second = divmod(combined, len(D))
+        return D(first), D(second)
+
     def test_skirmish_decision_values(self):
         """Test the integer values of skirmish decisions."""
-        assert SD.COOPERATE == 1
-        assert SD.ATTACK == 2
-        assert SD.NONE == 0
-        assert len(SD) == 3
+        assert D.COOPERATE == 1
+        assert D.ATTACK == 2
+        assert D.NONE == 0
+        assert len(D) == 3
 
     def test_skirmish_operation(self):
         """Test math operations to solve skirmish based on decisions.
         `combined = first * N + second`"""
 
-        assert SD.COOPERATE * len(SD) + SD.ATTACK == 5
-        assert SD.ATTACK * len(SD) + SD.COOPERATE == 7
-        assert SD.NONE * len(SD) + SD.COOPERATE == 1
-        assert SD.COOPERATE * len(SD) + SD.COOPERATE == 4
-        assert SD.NONE * len(SD) + SD.NONE == 0
+        assert self.operation(D.COOPERATE, D.COOPERATE) == SR.COOPERATION
+        assert self.operation(D.ATTACK, D.COOPERATE) == SR.TREASON_A
+        assert self.operation(D.COOPERATE, D.ATTACK) == SR.TREASON_B
+        assert self.operation(D.ATTACK, D.ATTACK) == SR.CONFLICT
+        assert self.operation(D.COOPERATE, D.NONE) == SR.ASSIMILATION_A
+        assert self.operation(D.NONE, D.COOPERATE) == SR.ASSIMILATION_B
+        assert self.operation(D.ATTACK, D.NONE) == SR.MASSACRE_A
+        assert self.operation(D.NONE, D.ATTACK) == SR.MASSACRE_B
+        assert self.operation(D.NONE, D.NONE) == SR.LOOSE
 
     def test_reverse_skirmish_operation(self):
         """Test reverse math operations to solve skirmish.
         `first, second = divmod(combined, N)`"""
-        assert divmod(5, len(SD)) == (SD.COOPERATE, SD.ATTACK,)
-        assert divmod(7, len(SD)) == (SD.ATTACK,  SD.COOPERATE,)
-        assert divmod(1, len(SD)) == (SD.NONE, SD.COOPERATE,)
-        assert divmod(4, len(SD)) == (SD.COOPERATE, SD.COOPERATE,)
-        assert divmod(0, len(SD)) == (SD.NONE, SD.NONE,)
+        assert self.reverse(SR.COOPERATION) == (D.COOPERATE, D.COOPERATE,)
+        assert self.reverse(SR.TREASON_A) == (D.ATTACK,  D.COOPERATE,)
+        assert self.reverse(SR.TREASON_B) == (D.COOPERATE, D.ATTACK,)
+        assert self.reverse(SR.CONFLICT) == (D.ATTACK, D.ATTACK,)
+        assert self.reverse(SR.ASSIMILATION_A) == (D.COOPERATE, D.NONE,)
+        assert self.reverse(SR.ASSIMILATION_B) == (D.NONE, D.COOPERATE,)
+        assert self.reverse(SR.MASSACRE_A) == (D.ATTACK, D.NONE,)
+        assert self.reverse(SR.MASSACRE_B) == (D.NONE, D.ATTACK,)
+        assert self.reverse(SR.LOOSE) == (D.NONE, D.NONE,)
