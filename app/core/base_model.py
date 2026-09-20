96d25e2ff4199aba4a34ad46cc5f2b2f0fae410d
Carlos Disher Luján Parra <cdlujanp@gmail.com>
2026-09-19 22:21:08 -0500

    feat: add config for suppor liskov in models pydantic

--- app/core/base_model.py
+++ app/core/base_model.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from ulid import ULID
 
 
 class CustomBaseModel(BaseModel):
     """Custom base that provides a centralized template for all models."""
     id: ULID = Field(default_factory=ULID)
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        revalidate_instances="never"
+    )
 
     def __hash__(self):
         return id(self)
